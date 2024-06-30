02c3258ebbfd86547641aa8b5674426494647ae2
pratap-nitjsr <127110382+pratap-nitjsr@users.noreply.github.com>
2024-06-30 10:17:37 +0530

    "."

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,7 +6,7 @@ export const sendToken = (user, statusCode, res, message) => {
     ),
     httpOnly: true,
     secure:true,
-    sameSite:'None';
+    sameSite:'None'
   };
 
   res.status(statusCode).cookie("token", token, options).json({
